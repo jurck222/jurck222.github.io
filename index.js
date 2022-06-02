6792d6423dc89c7b39857c000346646ed681765d
jurck222 <56190152+jurck222@users.noreply.github.com>
2022-06-02 15:59:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+var audio_file = document.getElementById("a");
+var out = document.getElementById("output");
+audio_file.addEventListener("change", function() {
+    var file = this.files[0];
+    var name = document.getElementById("name");
+    name.innerHTML="Title: "+file.name;
+    var reader = new FileReader();
+    reader.onload = function() {
+      console.log(reader.result+" fsfgsgsd");
+      var OfflineContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
+      var offlineContext = new OfflineContext(2, 30 * 44100, 44100);
+      offlineContext.decodeAudioData(reader.result, function(buffer) {
+        var min = Math.floor(buffer.duration/60);
+        var sec = Math.floor(buffer.duration%60);
+        console.log(min+":"+sec);
+        console.log(buffer);
+        var dur = document.getElementById("duration");
+        dur.innerHTML="Duration: "+min+"min and "+sec+"s"; 
+        var source = offlineContext.createBufferSource();
+        source.buffer = buffer;
+        var lowpass = offlineContext.createBiquadFilter();
+        lowpass.type = "lowpass";
+        lowpass.frequency.value = 150;
+        lowpass.Q.value = 1;
+        source.connect(lowpass);
+        var highpass = offlineContext.createBiquadFilter();
+        highpass.type = "highpass";
+        highpass.frequency.value = 100;
+        highpass.Q.value = 1;
+        lowpass.connect(highpass);
+        highpass.connect(offlineContext.destination);
+        source.start(0);
+        offlineContext.startRendering();
+    });
+    offlineContext.oncomplete = function(e) {
+      var buffer = e.renderedBuffer;
+      var peaks = getPeaks([buffer.getChannelData(0), buffer.getChannelData(1)]);
+      console.log(peaks);
+      var groups = getIntervals(peaks);
+      console.log(groups);
+      var top = groups.sort(function(intA, intB) {
+        return intB.count - intA.count;
+      }).splice(0, 5);
+      out.innerHTML="BPM: "+top[0].tempo;
+      console.log(top[0].tempo);
+    }
+  };
+    reader.readAsArrayBuffer(file);
+});
+  
+  function getPeaks(data) {
+    var partSize = 22050,
+        parts = data[0].length / partSize,
+        peaks = [];
+    console.log(data);
+    for (var i = 0; i < parts; i++) {
+      var max = 0;
+      for (var j = i * partSize; j < (i + 1) * partSize; j++) {
+        var volume = Math.max(Math.abs(data[0][j]), Math.abs(data[1][j]));
+        if (!max || (volume > max.volume)) {
+          max = {
+            position: j,
+            volume: volume
+          };
+        }
+      }
+      peaks.push(max);
+    }
+    peaks.sort(function(a, b) {
+      return b.volume - a.volume;
+    });
+  
+    peaks = peaks.splice(0, peaks.length * 0.5);
+    peaks.sort(function(a, b) {
+      return a.position - b.position;
+    });
+    return peaks;
+  }
+  function getIntervals(peaks) {
+  
+    var groups = [];
+  
+    peaks.forEach(function(peak, index) {
+      for (var i = 1; (index + i) < peaks.length && i < 10; i++) {
+        var group = {
+          tempo: (60 * 44100) / (peaks[index + i].position - peak.position),
+          count: 1
+        };
+  
+        while (group.tempo < 90) {
+          group.tempo *= 2;
+        }
+  
+        while (group.tempo > 180) {
+          group.tempo /= 2;
+        }
+  
+        group.tempo = Math.round(group.tempo);
+  
+        if (!(groups.some(function(interval) {
+          return (interval.tempo === group.tempo ? interval.count++ : 0);
+        }))) {
+          groups.push(group);
+        }
+      }
+    });
+    return groups;
+  }
+
+  
